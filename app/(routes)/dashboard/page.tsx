ea212fb4068e109e084e0548d32bd150fe505acc
Danilka_Akarawita <112841934+Danilka-Akarawita@users.noreply.github.com>
2024-11-23 00:42:10 +0530

    added user name in the dashboard

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,12 +1,17 @@
-import { UserButton } from '@clerk/nextjs'
+"use client"
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
+import CardInfo from './_components/CardInfo';
 
-const page = () => {
+function Dashboard() {
+  const {user}=useUser();
   return (
-    <div>
-      <UserButton/>
+    <div className='p-5'>
+      <h2 className='font-bold text-3xl'>Hi, {user?.fullName}</h2>
+      <p className='text-gray-500'>Here's what happening with your money </p>
+      <CardInfo/>
     </div>
   )
 }
 
-export default page
+export default Dashboard
