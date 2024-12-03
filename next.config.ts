d96c2a41030c1f26097c0d0b2fdc1d3c6db9ea3b
Yashu-x <n.thashenu@gmail.com>
2024-12-03 15:09:52 +0530

    dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+
+
+/**
+ * Run build or dev with SKIP_ENV_VALIDATION to skip env validation. This is especially useful
+ * for Docker builds.
+ */
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import("next").NextConfig} */
+const config = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    distDir: 'build',
+
 };
 
-export default nextConfig;
+export default config;
